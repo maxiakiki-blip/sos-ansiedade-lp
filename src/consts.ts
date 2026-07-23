9a56d0990945565b43c826b236e45ed533a38d7d
Maxi <maxiakiki@gmail.com>
2026-07-23 13:26:24 -0300

    Fijar cuota 12x (R$ 25,55) y checkout con oferta off

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const CHECKOUT_URL = 'https://pay.hotmart.com/L106226605F';
+export const CHECKOUT_URL = 'https://pay.hotmart.com/L106226605F?off=7vzsf4br';
 
 // Pixel de SOS Ansiedade (nuevo, no el 641071370521487 de Inquilino Online).
 export const META_PIXEL_ID = '1054129317181548';
